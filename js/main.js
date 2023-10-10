033fcdc6ada0aa5927fe0c8309c4308ec4c20b5c
authridoy <webkhridoy@gmail.com>
2023-10-10 14:55:56 +0600

    header navbar

---
+++ js/main.js
@@ -0,0 +1,81 @@
+// // const menuhamburger = document.querySelector(".menu-hamburger")
+// // const navlink = document.querySelector(".nav-links")
+// // menuhamburger.addEventlistener('click',()=>{
+// //     navlinks.classList.toggle('mobile-menu')
+// // })
+// // function toggleMenu() {
+// //     const toggleMenu = document.querySelector(".toggleMenu");
+// //     const navigation = document.querySelector(".navigation");
+// //     toggleMenu.classList.toggle(".active");
+// //     navigation.classList.toggle(".active");
+// // }
+// (function ($){
+
+//     "use-strict";
+
+//     $(document).ready(function(){
+//         // $(".case-studies-carousel").owlCarousel({
+//         //     items: 6, 
+//         //     margin: 30,
+//         //     loop: true,
+//         //     dots: false,
+//         //     nav: false,
+            
+//         // });
+//         // $(".testimonial-carousel").owlCarousel({
+//         //     items: 1, 
+//         //     margin: 0,
+//         //     loop: true,
+//         //     dots: false,
+//         //     nav: false,
+            
+//         // });
+//         // $(".home-page-slides").owlCarousel({
+//         //     items: 1, 
+//         //     margin: 0,
+//         //     loop: true,
+//         //     dots: false,
+//         //     nav: true,
+//         //     navText:["<span class='material-symbols-outlined'>arrow_back</span>", "<span class='material-symbols-outlined'>arrow_forward</span>"]
+            
+//         // });
+//         // $(".company-logo-carousel").owlCarousel({
+//         //     items: 6, 
+//         //     margin: 10,
+//         //     loop: true,
+//         //     dots: false,
+//         //     nav: false,
+            
+//         // });
+
+        
+//         // $(".menu-trigger").on('click',function(){
+//         //     $(".off-canvas-menu").addClass("show-off-canvas-menu");
+//         //     $(".of-canvas-menu-shade").addClass("active");
+//         // })
+
+//         // $(".menu-close, .of-canvas-menu-shade").on('click',function(){
+//         //     $(".off-canvas-menu").removeClass("show-off-canvas-menu");
+//         //     $(".of-canvas-menu-shade").removeClass("active");
+//         // })
+
+
+//         $(".menu-trigger").on('click',function(){
+//             $(".off-canvas-menu").addClass("show-off-canvas-menu");
+
+//         });
+//         $(".menu-close").on('click',function(){
+//             $(".off-canvas-menu").removeClass("show-off-canvas-menu");
+//         });
+        
+
+        
+//     });
+
+
+//     $(window).on("load",function (){
+
+//     });
+
+// } (jQuery));
+
